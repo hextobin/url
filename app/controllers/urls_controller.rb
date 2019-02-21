01015ecf4526efab5e9b68bfbf7a18f9834b7dcd
Spencer Brooks <spencer@databit.io>
2019-02-20 22:24:42 -0700

    show url on new page

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,11 +5,17 @@ class UrlsController < ApplicationController
 
   def create
     @url = Url.create(url_params)
+    redirect_to action: 'show', id: @url.id
+  end
+
+  def show
+    @url = Url.find(params[:id])
+    # binding.pry
   end
 
   private
     def url_params
-      params.require(:url).permit(:oldurl)
+      params.require(:url).permit(:oldurl, :id)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'urls/shorten'
-  resources :urls, only: [:create]
+  resources :urls, only: [:create, :show]
   # get 'urls/shorten'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
